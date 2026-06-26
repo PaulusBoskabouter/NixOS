768d01e970d568f65f0286ac08f653880d72a3ac
PaulusBoskabouter <43268349+PaulusBoskabouter@users.noreply.github.com>
2026-06-26 20:45:41 +0200

    update thing public key entry

--- modules/home/users/smurf/smurfUser.nix
+++ modules/home/users/smurf/smurfUser.nix
@@ -6,7 +6,7 @@
 				extraGroups = [ "networkmanager" "wheel" ];
 				hashedPasswordFile = config.age.secrets.passwdSmurf.path;
 				openssh.authorizedKeys.keys = [
-					"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDWvXdz5+8mHIbMJ0DYNZzTJm3e2A27oxUBri540rdx2 paul@calypso"
+                    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPXe1jU+e3JywNfLujZ6wulSxDLETotJ3F8Izu3jBmJX paul@calypso"
                     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDE7cFblt1Q/qV4WPSBIbhPWwfyRJlxRVUsH971eGlng paul@nautical"
 
 
