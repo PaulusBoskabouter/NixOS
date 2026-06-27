a9a3cba8eb9000de663a62f0bd259eee06a5b80e
PaulusBoskabouter <43268349+PaulusBoskabouter@users.noreply.github.com>
2026-06-27 18:14:57 +0200

    closed ssh for unknown devices on calypso

--- modules/hosts/calypso/configuration.nix
+++ modules/hosts/calypso/configuration.nix
@@ -9,7 +9,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "calypso"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -60,22 +59,23 @@
   nix.settings.experimental-features = ["nix-command" "flakes"];
 	
   # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true; # Stinky Nvidia
 	
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  # Enable the OpenSSH daemon to trusted devices only.
+  services.openssh= {
+  	enable = true;
+	settings = {
+		PasswordAuthentication = false;
+		PermitRootLogin = "no";
+		KbdInteractiveAuthentication = false;
+	};
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "26.05"; # Did you read the comment?
+  system.stateVersion = "26.05"; 
 
 };
 
