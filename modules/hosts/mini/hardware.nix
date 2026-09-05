ae89b199afaa3e3476c855e9434663bddde1031c
PaulusBoskabouter <43268349+PaulusBoskabouter@users.noreply.github.com>
2026-09-05 15:25:06 +0200

    finalize mini setup

--- modules/hosts/mini/hardware.nix
+++ modules/hosts/mini/hardware.nix
@@ -5,7 +5,30 @@
 flake.nixosModules.miniHardware = { config, lib, pkgs, modulesPath, ... }:
 
 {
+imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
 
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/6fe04e28-251e-426d-994c-d668aa021008";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/17FC-626D";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 };
 
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@ let
 	calypsokey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINfPMglUFTF9V71YpoobEqPxxLrnTDAAref9Avjz0FkR root@nixos";
 	serverkey= "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKsuaOpRKRDCKPhxnU36t85PcvuTWpnTlsjdnvLQyluG root@nixos";
 	userkey= "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPXe1jU+e3JywNfLujZ6wulSxDLETotJ3F8Izu3jBmJX paul@calypso";
-	minikey= "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO1rn6HySohKJS65a5vF4f7iOU+/sdUC3T7EWMNFliYx paul@nixos";
+	minikey= "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEadO+9WB6lAuMtN90TF0CzJ1HKdL+BZpE0Ue79E0SjX root@nixos";
 
 in {
 
