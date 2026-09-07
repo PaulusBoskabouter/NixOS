1b5f89b9e3d961937db6b766004ff3cf87cd464d
PaulusBoskabouter <43268349+PaulusBoskabouter@users.noreply.github.com>
2026-09-07 10:28:21 +0200

    Updated flake and removed LD_LIBRARY_PATH overwriting which caused bugs for other applications

--- modules/system/devToolkit.nix
+++ modules/system/devToolkit.nix
@@ -8,17 +8,6 @@
 			pkgs.cudaPackages.cudatoolkit
 			pkgs.virtualbox
 		];
-
-		environment.variables.LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [
-			pkgs.stdenv.cc.cc.lib
-			pkgs.zlib
-			pkgs.openssl
-			pkgs.glib
-			pkgs.cudaPackages.cudatoolkit
-			pkgs.cudaPackages.cuda_cudart
-			pkgs.libGL
-		]}:/run/opengl-driver/lib";
-
 		programs.nix-ld = {
 			enable = true;
 			libraries = with pkgs; [
