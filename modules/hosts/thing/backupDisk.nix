25f77116ad7ae5198184c3f23180f04c4bfdc5cc
PaulusBoskabouter <43268349+PaulusBoskabouter@users.noreply.github.com>
2026-09-11 11:40:17 +0200

    new uuid server disk

--- modules/hosts/thing/backupDisk.nix
+++ modules/hosts/thing/backupDisk.nix
@@ -1,7 +1,7 @@
 { self, inputs, ... }: {
 	flake.nixosModules.backupDisk = { config, pkgs, lib, ... }: {
 		fileSystems."/mnt/backup" = {
-			device = "/dev/disk/by-uuid/06980cbd-9047-4bd9-a404-139bb01ad357";
+			device = "/dev/disk/by-uuid/3b2c2665-8208-4790-8da6-de43158b8c28";
 			fsType = "ext4";
 		};
 	};
